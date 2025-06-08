11b7e72cf17969562752d99a9925dc38991c61ce
Bhavana B <bhavanasivaganga88@gmail.com>
2025-06-08 15:47:54 +0530

    Add files via upload

--- Bhavana/01_HelloWorld.js
+++ Bhavana/01_HelloWorld.js
@@ -1,1 +1,9 @@
-ftft
+// Question 1: Print "Hello, World!" to the console.
+
+
+function printHelloWorld() {
+  console.log("Hello, World!");
+}
+
+printHelloWorld(); 
+// Expected Output: Hello, World!
